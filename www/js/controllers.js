290f82ac7eb663e2b953fec8bf55e6e7bff13e8f
Rafael Telles <raftel.ti@gmail.com>
2015-09-15 16:29:16 -0300

    Add mana cost images

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,121 +1,139 @@
 angular.module('app.controllers', [])
-.controller('SearchController', function($scope,  $http){
-  var pendingTask;
+  .controller('SearchController', function ($scope, $http) {
+    var pendingTask;
 
-  $scope.updateSearch = function(){
-    setTimeout(function(){ requestByName(); }, 250);
-  };
-  
-  if($scope.search === undefined) {
-    $scope.search = "Pacifism";
-    requestByName();
-  }
-  
-  $scope.goToCard = function(id){
-  	window.location = "#/card/"+id;
-  }
+    $scope.updateSearch = function () {
+      setTimeout(function () {
+        requestByName();
+      }, 250);
+    };
 
-  $scope.goToMenu = function(){
-    window.location = "#/menu";
-  }
+    if ($scope.search === undefined) {
+      $scope.search = "Pacifism";
+      requestByName();
+    }
 
-  function requestByName() {
-  $http.get("https://api.deckbrew.com/mtg/cards?name="+$scope.search)
-    .success(function(response){
-      $scope.cards = response;
-    });}
-})
+    $scope.goToCard = function (id) {
+      window.location = "#/card/" + id;
+    }
 
-.controller('ScoreController', function($scope, $stateParams, $ionicActionSheet, $ionicPopup){
+    $scope.goToMenu = function () {
+      window.location = "#/menu";
+    }
+
+    function requestByName() {
+      $http.get("https://api.deckbrew.com/mtg/cards?name=" + $scope.search)
+        .success(function (response) {
+          $scope.cards = response;
+        });
+    }
+  })
+
+.controller('ScoreController', function ($scope, $stateParams, $ionicActionSheet, $ionicPopup) {
 
   $scope.players = [
-        {name:"Jace Beleren", life:20, infect:0},
-        {name:"Ajani", life:20, infect:0}
+    {
+      name: "Jace Beleren",
+      life: 20,
+      infect: 0
+    },
+    {
+      name: "Ajani",
+      life: 20,
+      infect: 0
+    }
   ];
 
-  $scope.changeLP = function(index,hit){
+  $scope.changeLP = function (index, hit) {
     $scope.players[index].life += hit;
-    if($scope.players[index].life < 0) $scope.players[index].life = 0;
+    if ($scope.players[index].life < 0) $scope.players[index].life = 0;
   }
 
-  $scope.resetLP = function(index){
+  $scope.resetLP = function (index) {
     $scope.players[index].life = 20;
   }
 
-  $scope.changeI = function(index,hit){
+  $scope.changeI = function (index, hit) {
     $scope.players[index].infect += hit;
-    if($scope.players[index].infect < 0) $scope.players[index].infect = 0;
-    if($scope.players[index].infect > 10) $scope.players[index].infect = 10;
+    if ($scope.players[index].infect < 0) $scope.players[index].infect = 0;
+    if ($scope.players[index].infect > 10) $scope.players[index].infect = 10;
   }
 
-  $scope.resetI = function(index){
+  $scope.resetI = function (index) {
     $scope.players[index].infect = 0;
   }
 
-  $scope.showOptions = function(){
+  $scope.showOptions = function () {
     var MatchOptions = $ionicActionSheet.show({
-      buttons:[
-        {text: 'One X One'}
+      buttons: [
+        {
+          text: 'One X One'
+        }
       ],
       titleText: 'Change Match Type',
       cancelText: 'Cancel',
-      cancel: function() {
+      cancel: function () {
 
       },
-      buttonClicked: function(index){
+      buttonClicked: function (index) {
         return true;
       }
     });
   }
 
- $scope.editNamePopup = function(index) {
+  $scope.editNamePopup = function (index) {
 
-   var popup = $ionicPopup.alert({
-     title: 'Change Name',
-     scope: $scope,
-     template: '<input ng-model="players['+index+'].name">'
-   });
- };
+    var popup = $ionicPopup.alert({
+      title: 'Change Name',
+      scope: $scope,
+      template: '<input ng-model="players[' + index + '].name">'
+    });
+  };
 
 
 })
 
 
-.controller('TransitionController', function($scope, $stateParams){
-  $scope.goToSearch = function(){
+.controller('TransitionController', function ($scope, $stateParams) {
+  $scope.goToSearch = function () {
     window.location = "#/search";
   }
 
-  $scope.goToMenu = function(){
+  $scope.goToMenu = function () {
     window.location = "#/menu";
   }
 
-  $scope.goToMatchManager = function(){
+  $scope.goToMatchManager = function () {
     window.location = "#/matchManager";
   }
 
-  $scope.goToScore = function(){
+  $scope.goToScore = function () {
     window.location = "#/score";
   }
 })
 
-.controller('CardController', function($scope,  $http, $stateParams){
+.controller('CardController', function ($scope, $http, $stateParams) {
 
-	$scope.cardId = $stateParams.id;
-	requestById();
+  $scope.cardId = $stateParams.id;
+  requestById();
 
-	function requestById() {
-	  	$http.get("https://api.deckbrew.com/mtg/cards/"+$scope.cardId)
-	    .success(function(response){
-	      $scope.card = response;
-        //console.log(response);
-    });}
+  function requestById() {
+    $http.get("https://api.deckbrew.com/mtg/cards/" + $scope.cardId)
+      .success(function (response) {
+        $scope.card = response;
 
-	$scope.goToSearch = function(){
-  	window.location = "#/search";
+        //Spliting mana cost string
+        $scope.card.cost = $scope.card.cost.split(/[{}]/).filter(function (e) {
+          return e
+        });
+      });
   }
 
-  $scope.goToMenu = function(){
+  $scope.goToSearch = function () {
+    window.location = "#/search";
+  }
+
+  $scope.goToMenu = function () {
     window.location = "#/menu";
   }
 });
